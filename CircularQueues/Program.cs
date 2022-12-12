8d8e88800344366ce920bdf23138ad3170ae5d61
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2022-12-12 17:42:00 +0700

    1

--- CircularQueues/Program.cs
+++ CircularQueues/Program.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace CircularQueues
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+
+        public Program()
         {
+            /*Initializing the value of the variables REAR and FRONT to -1 to indicate that
+             * the queue is initially empty.*/
+            FRONT = -1;
+            REAR = -1;
         }
     }
 }
